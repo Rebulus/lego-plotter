3ede8938f55f84512aab05e473292c73c5b52316
viking <viking@yandex-team.ru>
2014-11-29 19:37:13 +0300

    Приложенька для генерации букв на svg

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var app = express();
 app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.get('/', function(req, res) {
     res.render('index.jade');
+});
+
+app.post('/letter', function(req, res) {
+    console.log('BODY', req.body);
+    console.log('LETTER', req.param('letter'));
+    console.log('SVG', req.param('svg'));
+
+    fs.writeFile(__dirname + '/letters/' + req.param('letter') + '.svg', req.param('svg'), function(err) {
+        if (err) {
+            throw err;
+        }
+
+        console.log('OK');
+
+        res.redirect('/');
+    });
 });
 
 var server = app.listen(3000, function() {
     console.log('ok');
 });
+
